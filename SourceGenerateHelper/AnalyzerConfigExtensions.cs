5e661679767ee98505ec6c4afb9d8b40d47963c2
usausa <machi.pon@gmail.com>
2026-05-02 11:17:43 +0900

    Update

--- SourceGenerateHelper/AnalyzerConfigExtensions.cs
+++ SourceGenerateHelper/AnalyzerConfigExtensions.cs
@@ -21,40 +21,7 @@ public static class AnalyzerConfigExtensions
                 return default!;
             }
 
-            var targetType = Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T);
-
-            if (targetType == typeof(int))
-            {
-                var result = int.Parse(value, CultureInfo.InvariantCulture);
-                return Unsafe.As<int, T>(ref result);
-            }
-            if (targetType == typeof(bool))
-            {
-                var result = bool.Parse(value);
-                return Unsafe.As<bool, T>(ref result);
-            }
-            if (targetType == typeof(long))
-            {
-                var result = long.Parse(value, CultureInfo.InvariantCulture);
-                return Unsafe.As<long, T>(ref result);
-            }
-            if (targetType == typeof(double))
-            {
-                var result = double.Parse(value, CultureInfo.InvariantCulture);
-                return Unsafe.As<double, T>(ref result);
-            }
-            if (targetType == typeof(float))
-            {
-                var result = float.Parse(value, CultureInfo.InvariantCulture);
-                return Unsafe.As<float, T>(ref result);
-            }
-            if (targetType == typeof(decimal))
-            {
-                var result = decimal.Parse(value, CultureInfo.InvariantCulture);
-                return Unsafe.As<decimal, T>(ref result);
-            }
-
-            return (T)Convert.ChangeType(value, targetType, CultureInfo.InvariantCulture);
+            return (T)Convert.ChangeType(value, Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T), CultureInfo.InvariantCulture);
         }
 
         return default!;
