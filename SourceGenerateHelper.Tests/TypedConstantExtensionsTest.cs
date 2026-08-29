f03a705597a5ee26a51d1846759fa4fdf5742058
usausa <machi.pon@gmail.com>
2026-08-29 12:03:34 +0900

    Update

--- SourceGenerateHelper.Tests/TypedConstantExtensionsTest.cs
+++ SourceGenerateHelper.Tests/TypedConstantExtensionsTest.cs
@@ -11,16 +11,26 @@ public sealed class TypedConstantExtensionsTest
 
         public sealed class ValAttribute : Attribute
         {
+            public ValAttribute(int v) { }
             public ValAttribute(long v) { }
             public ValAttribute(uint v) { }
             public ValAttribute(ulong v) { }
             public ValAttribute(float v) { }
             public ValAttribute(double v) { }
+            public ValAttribute(char v) { }
+            public ValAttribute(bool v) { }
             public ValAttribute(string v) { }
             public ValAttribute(Sample v) { }
+            public ValAttribute(Flag v) { }
+            public ValAttribute(Type v) { }
+            public ValAttribute(object v) { }
+            public ValAttribute(int[] v) { }
         }
 
         public enum Sample { A, B }
+
+        [Flags]
+        public enum Flag { None = 0, A = 1, B = 2 }
 
         [Val(123L)] public class CLong { }
         [Val(5u)] public class CUInt { }
@@ -29,18 +39,39 @@ public sealed class TypedConstantExtensionsTest
         [Val(1.5d)] public class CDouble { }
         [Val("x")] public class CString { }
         [Val(Sample.B)] public class CEnum { }
+        [Val(Double.NaN)] public class CNaN { }
+        [Val(Double.PositiveInfinity)] public class CInfinity { }
+        [Val(Single.NaN)] public class CFloatNaN { }
+        [Val('a')] public class CChar { }
+        [Val(true)] public class CBool { }
+        [Val("a\"b\nc")] public class CEscape { }
+        [Val((Flag)(Flag.A | Flag.B))] public class CFlags { }
+        [Val(typeof(String))] public class CType { }
+        [Val((object)null)] public class CNull { }
+        [Val(new int[] { 1, 2 })] public class CArray { }
+        [Val(-1)] public class CNegative { }
+        [Val(1)] public class CInt { }
         """;
+
+    // Symbols are compared by identity, so all symbols must come from the same compilation
+    private static readonly CSharpCompilation Compilation = CSharpCompilation.Create(
+        "TestAssembly",
+        [CSharpSyntaxTree.ParseText(Source)],
+        [MetadataReference.CreateFromFile(typeof(object).Assembly.Location)],
+        new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
 
     private static TypedConstant GetConstant(string typeName)
     {
-        var compilation = CSharpCompilation.Create(
-            "TestAssembly",
-            [CSharpSyntaxTree.ParseText(Source)],
-            [MetadataReference.CreateFromFile(typeof(object).Assembly.Location)],
-            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-        var type = compilation.GetTypeByMetadataName(typeName)!;
+        var type = Compilation.GetTypeByMetadataName(typeName)!;
         return type.GetAttributes().First().ConstructorArguments[0];
     }
+
+    private static ITypeSymbol GetSpecialType(SpecialType specialType) =>
+        Compilation.GetSpecialType(specialType);
+
+    // ------------------------------------------------------------
+    // Postfix
+    // ------------------------------------------------------------
 
     [Fact]
     public void LongHasPostfix()
@@ -86,5 +117,103 @@ public sealed class TypedConstantExtensionsTest
         var constant = GetConstant("CEnum");
 
         Assert.Equal(constant.ToCSharpString(), constant.ToCSharpStringWithPostfix());
+    }
+
+    [Fact]
+    public void PostfixForNotFiniteIsValidCSharp()
+    {
+        Assert.Equal("double.NaN", GetConstant("CNaN").ToCSharpStringWithPostfix());
+        Assert.Equal("double.PositiveInfinity", GetConstant("CInfinity").ToCSharpStringWithPostfix());
+        Assert.Equal("float.NaN", GetConstant("CFloatNaN").ToCSharpStringWithPostfix());
+    }
+
+    // ------------------------------------------------------------
+    // Expression
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void PrimitiveExpression()
+    {
+        Assert.Equal("1", GetConstant("CInt").ToCSharpExpression());
+        Assert.Equal("123L", GetConstant("CLong").ToCSharpExpression());
+        Assert.Equal("5u", GetConstant("CUInt").ToCSharpExpression());
+        Assert.Equal("5uL", GetConstant("CULong").ToCSharpExpression());
+        Assert.Equal("1.5f", GetConstant("CFloat").ToCSharpExpression());
+        Assert.Equal("1.5d", GetConstant("CDouble").ToCSharpExpression());
+        Assert.Equal("true", GetConstant("CBool").ToCSharpExpression());
+        Assert.Equal("'a'", GetConstant("CChar").ToCSharpExpression());
+        Assert.Equal("\"x\"", GetConstant("CString").ToCSharpExpression());
+    }
+
+    [Fact]
+    public void StringExpressionIsEscaped()
+    {
+        Assert.Equal("\"a\\\"b\\nc\"", GetConstant("CEscape").ToCSharpExpression());
+    }
+
+    [Fact]
+    public void NotFiniteExpressionIsValidCSharp()
+    {
+        Assert.Equal("double.NaN", GetConstant("CNaN").ToCSharpExpression());
+        Assert.Equal("double.PositiveInfinity", GetConstant("CInfinity").ToCSharpExpression());
+        Assert.Equal("float.NaN", GetConstant("CFloatNaN").ToCSharpExpression());
+    }
+
+    [Fact]
+    public void NullExpression()
+    {
+        Assert.Equal("null", GetConstant("CNull").ToCSharpExpression());
+    }
+
+    [Fact]
+    public void EnumExpressionUsesMemberName()
+    {
+        Assert.Equal("global::Sample.B", GetConstant("CEnum").ToCSharpExpression());
+    }
+
+    [Fact]
+    public void CombinedFlagsExpressionUsesCast()
+    {
+        Assert.Equal("(global::Flag)(3)", GetConstant("CFlags").ToCSharpExpression());
+    }
+
+    [Fact]
+    public void TypeExpressionUsesTypeof()
+    {
+        Assert.Equal("typeof(string)", GetConstant("CType").ToCSharpExpression());
+    }
+
+    [Fact]
+    public void ArrayExpressionIsNotSupported()
+    {
+        Assert.Null(GetConstant("CArray").ToCSharpExpression());
+    }
+
+    // ------------------------------------------------------------
+    // Target type
+    // ------------------------------------------------------------
+
+    [Fact]
+    public void DifferentTargetTypeIsCast()
+    {
+        Assert.Equal("(double)1", GetConstant("CInt").ToCSharpExpression(GetSpecialType(SpecialType.System_Double)));
+    }
+
+    [Fact]
+    public void NegativeValueIsParenthesized()
+    {
+        Assert.Equal("(double)(-1)", GetConstant("CNegative").ToCSharpExpression(GetSpecialType(SpecialType.System_Double)));
+    }
+
+    [Fact]
+    public void SameTargetTypeIsNotCast()
+    {
+        Assert.Equal("1", GetConstant("CInt").ToCSharpExpression(GetSpecialType(SpecialType.System_Int32)));
+    }
+
+    [Fact]
+    public void EnumIsNotCastToTargetType()
+    {
+        Assert.Equal("global::Sample.B", GetConstant("CEnum").ToCSharpExpression(GetSpecialType(SpecialType.System_Object)));
     }
 }
--- SourceGenerateHelper/TypedConstantExtensions.cs
+++ SourceGenerateHelper/TypedConstantExtensions.cs
@@ -1,12 +1,31 @@
 namespace SourceGenerateHelper;
 
+using System;
+using System.Globalization;
+
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
 public static class TypedConstantExtensions
 {
+    // ------------------------------------------------------------
+    // Convert
+    // ------------------------------------------------------------
+
     public static string ToCSharpStringWithPostfix(this TypedConstant constant)
     {
+        // Roslyn formats special values as NaN/Infinity, which are not valid C#
+        if (constant.Kind == TypedConstantKind.Primitive)
+        {
+            switch (constant.Value)
+            {
+                case float f:
+                    return FormatSingle(f);
+                case double d:
+                    return FormatDouble(d);
+            }
+        }
+
         var str = constant.ToCSharpString();
         return constant.Type?.SpecialType switch
         {
@@ -18,5 +37,118 @@ public static class TypedConstantExtensions
             SpecialType.System_Decimal => $"{str}m",
             _ => str
         };
+    }
+
+    public static string? ToCSharpExpression(this TypedConstant constant, ITypeSymbol? targetType = null)
+    {
+        if (constant.IsNull)
+        {
+            return "null";
+        }
+
+        var expression = MakeExpression(constant);
+        if (expression is null)
+        {
+            return null;
+        }
+
+        if ((targetType is null) ||
+            (constant.Kind != TypedConstantKind.Primitive) ||
+            (constant.Type is null) ||
+            SymbolEqualityComparer.Default.Equals(constant.Type, targetType))
+        {
+            return expression;
+        }
+
+        var targetTypeName = targetType.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+        return expression[0] == '-'
+            ? $"({targetTypeName})({expression})"
+            : $"({targetTypeName}){expression}";
+    }
+
+    // ------------------------------------------------------------
+    // Helper
+    // ------------------------------------------------------------
+
+    private static string? MakeExpression(TypedConstant constant) =>
+        constant.Kind switch
+        {
+            TypedConstantKind.Primitive => MakePrimitiveExpression(constant.Value),
+            TypedConstantKind.Enum => MakeEnumExpression(constant),
+            TypedConstantKind.Type => $"typeof({((ITypeSymbol)constant.Value!).ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat)})",
+            _ => null
+        };
+
+    private static string? MakePrimitiveExpression(object? value) =>
+        value switch
+        {
+            string s => SymbolDisplay.FormatLiteral(s, true),
+            char c => SymbolDisplay.FormatLiteral(c, true),
+            bool b => b ? "true" : "false",
+            float f => FormatSingle(f),
+            double d => FormatDouble(d),
+            decimal m => m.ToString(CultureInfo.InvariantCulture) + "m",
+            long l => l.ToString(CultureInfo.InvariantCulture) + "L",
+            ulong ul => ul.ToString(CultureInfo.InvariantCulture) + "uL",
+            uint ui => ui.ToString(CultureInfo.InvariantCulture) + "u",
+            IFormattable formattable => formattable.ToString(null, CultureInfo.InvariantCulture),
+            _ => null
+        };
+
+    private static string MakeEnumExpression(TypedConstant constant)
+    {
+        var typeName = constant.Type!.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
+
+        // A combined flags value has no single member name, so a cast is used instead
+        foreach (var member in constant.Type.GetMembers())
+        {
+            if ((member is IFieldSymbol { IsConst: true, HasConstantValue: true } field) &&
+                Equals(field.ConstantValue, constant.Value))
+            {
+                return $"{typeName}.{field.Name}";
+            }
+        }
+
+        return $"({typeName})({((IFormattable)constant.Value!).ToString(null, CultureInfo.InvariantCulture)})";
+    }
+
+    private static string FormatSingle(float value)
+    {
+        if (Single.IsNaN(value))
+        {
+            return "float.NaN";
+        }
+
+        if (Single.IsPositiveInfinity(value))
+        {
+            return "float.PositiveInfinity";
+        }
+
+        if (Single.IsNegativeInfinity(value))
+        {
+            return "float.NegativeInfinity";
+        }
+
+        return value.ToString("R", CultureInfo.InvariantCulture) + "f";
+    }
+
+    private static string FormatDouble(double value)
+    {
+        if (Double.IsNaN(value))
+        {
+            return "double.NaN";
+        }
+
+        if (Double.IsPositiveInfinity(value))
+        {
+            return "double.PositiveInfinity";
+        }
+
+        if (Double.IsNegativeInfinity(value))
+        {
+            return "double.NegativeInfinity";
+        }
+
+        return value.ToString("R", CultureInfo.InvariantCulture) + "d";
     }
 }
