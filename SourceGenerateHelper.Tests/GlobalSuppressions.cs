0f3cfb8438c4c7c0c4016eb9a1f78364f97f581e
usausa <machi.pon@gmail.com>
2026-08-30 15:23:40 +0900

    Update

--- SourceGenerateHelper.Tests/GlobalSuppressions.cs
+++ SourceGenerateHelper.Tests/GlobalSuppressions.cs
@@ -1,2 +1,1 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "Ignore")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("MicrosoftCodeAnalysisReleaseTracking", "RS2008:Enable analyzer release tracking", Justification = "Ignore")]
